7fd130bda49fd6944314164e14bba588329fcaa2
2Sravs <sravani.chekuri1243#gmail.com>
2023-09-07 23:11:57 +0530

    firstcommit

---
+++ Views/Home/CustomHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace CustomHelper.Views.Home
+{
+    public class CustomHelper.View.Home
+    {
+        public static HtmlString File(string id)
+        {
+            TagBuilder tb = new TagBuilder("input");
+            tb.Attributes.Add("type", "file");
+            tb.Attributes.Add("id", id);
+            return new MvcHtmlString(tb.ToString());
+        }
+    }
+}
+
+namespace CustomHelper
+{
+    class MvcHtmlString : HtmlString
+    {
+        public MvcHtmlString(string? value) : base(value)
+        {
+        }
+    }
+}
